1edaeef8cb4b91b8465d86c0dad530a96c4eaaf9
nicolas monmerque <nicolas.monmerque@gmail.com>
2022-11-17 10:41:18 +0100

    Rails Yelp MVP a finir

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant_id = @restaurant.id
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
